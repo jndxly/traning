630fb6dc0ce5268b5e1437f9acfc3a02848d18e0
chenhao278 <chenhao278@jd.com>
2022-05-27 09:11:48 +0800

    feat: 转码

---
+++ getEncodeString.js
@@ -0,0 +1,42 @@
+/**
+ * // Excel的列命名规则，将整数的编号转换成字符串的形式。
+// 给出一个数 
+// index，令 
+// 1=A, 
+// 2=B，
+// 26=Z，
+// 27=AA ... 最后 
+// 702=ZZ，每行最多有 
+// 702 个编号。当 
+// index>702 时，需要换一行，并重新开始编号，并重新从 
+// A 开始编号，如 
+// 703=A，
+// 705=C。每满 
+// 702 个数都要换行，重新开始编号。
+// 最后转换的结果为 
+// index 所在的行号加上他的编号。
+// 如最终 
+// 1=1A,2=1B,3=1C,26=1Z,27=1AA,702=1ZZ,705=2C,1404=2ZZ。
+// 如 705 在第 2 行，编号为 C，所以它通过转换得到的字符串是 2C。
+ */
+function getStr(num){
+    if(num <= 0) return ''
+    const index = parseInt(num)
+    
+    const row  = Math.ceil(index / 702)
+    const col = index - (row - 1) * 702
+    const character = getCol(col)
+    console.log(row.toString() + character)
+    return row.toString() + character
+}
+function getCol(col){
+    const first = Math.floor(col / 27)
+    const left = col - first * 26
+    return getCharacter(first) + getCharacter(left)
+    
+}
+function getCharacter(col){
+    const arr = ['','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    return arr[col]
+}
+getStr(702)
