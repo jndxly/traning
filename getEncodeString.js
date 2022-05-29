4673b9ba24b6b40ef28b8c72c8ee7adf4c8bc817
chenhao278 <chenhao278@jd.com>
2022-05-29 09:59:33 +0800

    feat: getencoding

--- getEncodeString.js
+++ getEncodeString.js
@@ -6,6 +6,7 @@
 // 2=B，
 // 26=Z，
 // 27=AA ... 最后 
+// ... az 52 53 = ba
 // 702=ZZ，每行最多有 
 // 702 个编号。当 
 // index>702 时，需要换一行，并重新开始编号，并重新从 
@@ -18,7 +19,21 @@
 // 如最终 
 // 1=1A,2=1B,3=1C,26=1Z,27=1AA,702=1ZZ,705=2C,1404=2ZZ。
 // 如 705 在第 2 行，编号为 C，所以它通过转换得到的字符串是 2C。
+
+52 - 26 = 26
+26 / 26 = 1
+
+53 - 26 = 27
+27/26 = 2
+
+1 - 26  a-z
+1*26 + 1 - 1*26 + 26 aa-az
+...
+26*26 + 1 - 26*26 + 26 za - zz
+
  */
+
+
 function getStr(num){
     if(num <= 0) return ''
     const index = parseInt(num)
@@ -27,16 +42,24 @@ function getStr(num){
     const col = index - (row - 1) * 702
     const character = getCol(col)
     console.log(row.toString() + character)
-    return row.toString() + character
+    return '' + row + character
 }
 function getCol(col){
-    const first = Math.floor(col / 27)
-    const left = col - first * 26
-    return getCharacter(first) + getCharacter(left)
+    let first = 0, second = ''
+    if(col <= 26){
+        second = col
+    }
+    else{
+        first = Math.ceil((col - 26) / 26)
+        second = col - first * 26
+    }
+    // const first = Math.floor(col / 26) - 1
+    // const left = col - first * 26
+    return getCharacter(first) + getCharacter(second)
     
 }
 function getCharacter(col){
     const arr = ['','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     return arr[col]
 }
-getStr(702)
+getStr(1404)
